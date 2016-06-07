937f6498fff7ceb661e1db40e8a84b890045bb92
Joe Nelson <begriffs@users.noreply.github.com>
2016-06-07 09:53:46 -0700

    Customize theme on readthedocs (#75)
    * Rename theme so that RTD knows we mean it
    * Hide RTD ads

--- conf.py
+++ conf.py
@@ -110,7 +110,13 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'citus'
+html_theme_path = ['_themes',]
+html_context = {
+  'css_files': [
+    '_static/css/theme.css',
+  ]
+}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
