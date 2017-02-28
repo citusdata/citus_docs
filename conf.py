49dc50a360e9193c30d9107273e25486e3ef5743
Joe Nelson <begriffs@users.noreply.github.com>
2017-02-28 09:10:45 -0800

    Bump copyright (#298)

--- conf.py
+++ conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Citus'
-copyright = u'2015, Citus Data'
+copyright = u'2017, Citus Data'
 author = u'Citus Data'
 
 # The version info for the project you're documenting, acts as replacement for
