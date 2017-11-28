06e97703a594cec095b9e7783e5f46a060bc3d16
Joe Nelson <cred+github@begriffs.com>
2017-11-28 15:38:44 -0600

    Use sphinx_rtd_theme via pip rather than duping it
    Now using sphinx_rtd_theme v0.2.4 with the ability to more easily
    upgrade it independently of our own styling customizations.
    
    This commit resets the style of our docs to a vanilla RTD look.

--- conf.py
+++ conf.py
@@ -110,11 +110,12 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'citus'
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
 html_theme_path = ['_themes',]
 html_context = {
   'css_files': [
-    '_static/css/theme.css',
+    '_static/css/citus.css',
     '_static/css/flexboxgrid.min.css',
   ],
   'canonical_urls': {
