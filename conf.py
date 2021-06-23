72a38295974d0a482d13d35743e578b61170bffa
Joe Nelson <jonels@microsoft.com>
2021-06-23 11:57:07 -0500

    Switch to our fork of sphinx_rtd_theme

--- conf.py
+++ conf.py
@@ -111,8 +111,8 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-import sphinx_rtd_theme
-html_theme = 'sphinx_rtd_theme'
+import sphinx_rtd_theme_citus
+html_theme = 'sphinx_rtd_theme_citus'
 html_theme_options = {
 
   ################################################################
